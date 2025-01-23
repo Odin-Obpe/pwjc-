90cdca05506a5225ea80385189338b2f4d5f90e5
bbryla <01169007@pw.edu.pl>
2025-01-23 18:01:25 +0100

    Adjusted game.cpp to work with multiple threads

--- Game.cpp
+++ Game.cpp
@@ -1,16 +1,37 @@
 #include "Game.h"
 #include "Player.h"
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
 
-void Game::play(){
-    for(int i = 0; i < this->NumberOfRounds; i++){
-        for(int j = 0; j < this->Players.size(); j++){
-            for(int k = j + 1; k < this->Players.size(); k++){
-                this->Players[j]->fight(this->Players[k]);
-            }
+std::mutex mtx;
+
+void runRound(Game* game, int roundNumber) {
+    std::lock_guard<std::mutex> lock(mtx);
+    for (int j = 0; j < game->Players.size(); j++) {
+        for (int k = j + 1; k < game->Players.size(); k++) {
+            game->Players[j]->fight(game->Players[k]);
         }
     }
-    for(int i = 0; i < this->Players.size(); i++){
-        std::cout << this->Players[i]->Id << " " << this->Players[i]->strategy->name << " " << this->Players[i]->points << std::endl;
+}
+
+void Game::play() {
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < this->NumberOfRounds; i++) {
+        threads.push_back(std::thread(runRound, this, i));
+    }
+
+
+    for (auto& t : threads) {
+        t.join();
+    }
+    threads.clear();
+
+
+    std::lock_guard<std::mutex> lock(mtx);
+    for (auto player : this->Players) {
+        std::cout << "Player " << player->Id << " points: " << player->points << std::endl;
     }
 }
